8426718e8ed3ae69ba1ccb686e4d49832bc7042c
Paulo Robert Lima Gomes <paulorobert5776@gmail.com>
2024-10-07 17:22:57 -0300

    Inserção de imagens na mini janela

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const modalBtns = document.querySelectorAll('.modal-btn');
 const modal = document.getElementById('modal');
 const modalDate = document.getElementById('modal-date');
 const modalAddress = document.getElementById('modal-address');
+const modalImage = document.getElementById('modal-image');
 const closeModal = document.querySelector('.close-btn');
 
 modalBtns.forEach(btn => {
   btn.addEventListener('click', () => {
     const date = btn.getAttribute('data-date');
     const address = btn.getAttribute('data-address');
+    const image = btn.getAttribute('data-image');
     
     modalDate.textContent = `Data e Hora: ${date}`;
     modalAddress.textContent = `Endereço: ${address}`;
+    modalImage.src = image;
     modal.style.display = 'block';
   });
 });
